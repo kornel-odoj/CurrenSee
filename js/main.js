16095a9a7d596921d92e9ac72432131059133ec4
kornel-odoj <kornelodoj.4@gmail.com>
2023-03-24 00:48:02 +0100

    Added suggested improvements

--- js/main.js
+++ js/main.js
@@ -1,43 +1,28 @@
 const nbpApi = 'https://api.nbp.pl/api/exchangerates/tables/A/?format=json';
 const btn = document.querySelector('.input-button');
-const inputField = document.querySelector('.input-name')
-
+const inputField = document.querySelector('.input-name');
+const currencySelect = document.querySelector('.currency-select');
 const fetchCurrencies = async () => {
-    const result1 = await fetch(nbpApi);
-    const currenciesObject = await result1.json();
+    const result = await fetch(nbpApi);
+    const currenciesObject = await result.json();
     const rates = currenciesObject[0].rates;
     return rates;
 };
 
 const calculateCurrencies = async () => {
-    const result = await fetchCurrencies();
-    let EUR, USD, CHF;
-    const input = parseFloat(inputField.value);
-    const select = document.querySelector('.currency-select').value;
-    const paragraphResult  = document.querySelector('.result');
+    const inputValue = parseFloat(inputField.value);
+    const selectValue = currencySelect.value;
+    const paragraphResult = document.querySelector('.result');
     let calculated = 0;
-
-    for (i = 0; i < result.length; i++) {
-        if (result[i].code === 'EUR') {
-            EUR = result[i].mid;
-        } else if (result[i].code === 'USD') {
-            USD = result[i].mid;
-        } else if (result[i].code === 'CHF') {
-            CHF = result[i].mid;
-        }
+    
+    if(!isNaN(inputValue) && inputValue > 0) {
+        const result = await fetchCurrencies();
+        const rate = result.find(({ code }) => code === selectValue).mid;
+        calculated = (inputValue * rate).toFixed(2);
+        paragraphResult.innerText = `${inputValue} ${selectValue} to ${calculated} PLN`;
     }
 
-    if (!isNaN(input) && input > 0){
-        if(select == "EUR"){
-            calculated = input * EUR;
-        }else if(select == "USD"){
-            calculated = input * USD;
-        }else if(select == "CHF"){
-            calculated = input * CHF;
-        }
-        calculated = calculated.toFixed(2);
-        paragraphResult.innerText = `${input} ${select} to ${calculated} PLN`;
-    }else{
+    else{
         swal({
             title: "Kwota musi być liczbą dodatnią",
             text: "Wprowadź odpowiednią wartość.",
@@ -47,10 +32,8 @@ const calculateCurrencies = async () => {
     }
 };
 
-const currencySelect = document.querySelector('.currency-select');
 currencySelect.addEventListener('change', () => {
-    const selectedCurrency = currencySelect.value;
-    const queryString = `?currencies=${selectedCurrency}#`;
+    const queryString = `?currencies=${currencySelect.value}#`;
     window.history.replaceState(null, null, queryString);
 });
 
